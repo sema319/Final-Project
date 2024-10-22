1abf30f46248fad5e3d1b10a6cdbd3b5ae87f072
sema319 <semaf319@gmail.com>
2024-10-22 10:18:54 +0300

    adding edit profile to views File

---
+++ lib/views/EditProfile.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+
+
+class Homepagescreen extends StatefulWidget {
+  const Homepagescreen({super.key, required this.title});
+
+
+
+  final String title;
+
+  @override
+  State<Homepagescreen> createState() => HomepagescreenPageState();
+}
+
+class HomepagescreenPageState extends State<Homepagescreen> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+
+
+
+
+
+
+
+          ],
+        ),
+      ),
+
+    );
+  }
+}
