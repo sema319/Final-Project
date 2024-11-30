ca356c9d330a4f9e79dd9c747554ee5e4f70d090
sema319 <semaf319@gmail.com>
2024-11-30 10:51:46 +0200

    Add files via upload

---
+++ sema12 (1).sql
@@ -0,0 +1,199 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 30, 2024 at 09:52 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sema12`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `business`
+--
+
+CREATE TABLE `business` (
+  `businessID` int(11) NOT NULL,
+  `businessName` varchar(500) NOT NULL,
+  `address` varchar(500) NOT NULL,
+  `phone1` varchar(15) NOT NULL,
+  `phone2` varchar(15) NOT NULL,
+  `capcity` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  `businessTypeID` int(11) NOT NULL,
+  `createdDateTime` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `business`
+--
+
+INSERT INTO `business` (`businessID`, `businessName`, `address`, `phone1`, `phone2`, `capcity`, `price`, `businessTypeID`, `createdDateTime`) VALUES
+(1, 'lalune', 'baqa', '0463333223', '0521231231', 300, 4000, 6, '2024-11-30 08:51:28'),
+(2, 'alnor', 'baqa', '0463333223', '0521231231', 500, 4500, 6, '2024-11-30 08:51:28');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `businesstypes`
+--
+
+CREATE TABLE `businesstypes` (
+  `businessTypeID` int(11) NOT NULL,
+  `businessType` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `businesstypes`
+--
+
+INSERT INTO `businesstypes` (`businessTypeID`, `businessType`) VALUES
+(1, 'decration'),
+(2, 'dj'),
+(3, 'fruits'),
+(4, 'makeup'),
+(5, 'sweets'),
+(6, 'weddingHall');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clients`
+--
+
+CREATE TABLE `clients` (
+  `clientID` int(11) NOT NULL,
+  `clientName` varchar(500) NOT NULL,
+  `phoneNumber` varchar(500) NOT NULL,
+  `email` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `clients`
+--
+
+INSERT INTO `clients` (`clientID`, `clientName`, `phoneNumber`, `email`) VALUES
+(1, 'Sema', '0503332211', 'ddddd@gamil.com'),
+(2, 'adan', '0503332211', 'ddsssdd@gamil.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `paymentstypes`
+--
+
+CREATE TABLE `paymentstypes` (
+  `paymentID` int(11) NOT NULL,
+  `paymentTypeName` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `paymentstypes`
+--
+
+INSERT INTO `paymentstypes` (`paymentID`, `paymentTypeName`) VALUES
+(1, 'כרטיס אשראי'),
+(2, 'ציקם');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `userID` int(11) NOT NULL,
+  `firstName` varchar(500) NOT NULL,
+  `lastName` varchar(500) NOT NULL,
+  `password` varchar(500) NOT NULL,
+  `phoneNumber` int(11) NOT NULL,
+  `createdDateTime` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `business`
+--
+ALTER TABLE `business`
+  ADD PRIMARY KEY (`businessID`);
+
+--
+-- Indexes for table `businesstypes`
+--
+ALTER TABLE `businesstypes`
+  ADD PRIMARY KEY (`businessTypeID`);
+
+--
+-- Indexes for table `clients`
+--
+ALTER TABLE `clients`
+  ADD PRIMARY KEY (`clientID`);
+
+--
+-- Indexes for table `paymentstypes`
+--
+ALTER TABLE `paymentstypes`
+  ADD PRIMARY KEY (`paymentID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `business`
+--
+ALTER TABLE `business`
+  MODIFY `businessID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `businesstypes`
+--
+ALTER TABLE `businesstypes`
+  MODIFY `businessTypeID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `clients`
+--
+ALTER TABLE `clients`
+  MODIFY `clientID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `paymentstypes`
+--
+ALTER TABLE `paymentstypes`
+  MODIFY `paymentID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
